ee9993abe41a3bc3c68a46e0f173ebf463fda8e6
Matheus <matheus.loureiro@aluno.impacta.edu.br>
2026-05-01 19:33:24 -0300

    Corrige indice duplicado do slug

--- loja/migrations/0012_seo_security_fields.py
+++ loja/migrations/0012_seo_security_fields.py
@@ -35,7 +35,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='produto',
             name='slug',
-            field=models.SlugField(blank=True, max_length=140, null=True),
+            field=models.SlugField(blank=True, db_index=False, max_length=140, null=True),
         ),
         migrations.AddField(
             model_name='produto',
