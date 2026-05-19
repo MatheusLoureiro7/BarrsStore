979f24ecd4e8b65b6ea5ec694ed0daa7d361dfbc
Matheus <matheus.loureiro@aluno.impacta.edu.br>
2026-05-19 18:14:49 -0300

    Restaura visual com fallback de CSS inline

---
+++ loja/templatetags/inline_static.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+from pathlib import Path
+
+from django import template
+from django.contrib.staticfiles import finders
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+
+@lru_cache(maxsize=64)
+def _read_static_file(path):
+    found_path = finders.find(path)
+    if not found_path:
+        return ''
+    return Path(found_path).read_text(encoding='utf-8')
+
+
+@register.simple_tag
+def inline_static(path):
+    return mark_safe(_read_static_file(path))
