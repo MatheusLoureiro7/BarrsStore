6ca414c3bff9b0cd407cc80c5fd452995746cb65
Matheusbibico <bibibico@gmail.com>
2026-05-27 12:32:52 -0300

    Fix CSP para busca de CEP

--- barrs_store/settings.py
+++ barrs_store/settings.py
@@ -233,7 +233,7 @@ CONTENT_SECURITY_POLICY = os.environ.get(
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
     "font-src 'self' data: https://fonts.gstatic.com; "
     "img-src 'self' data: blob: https://res.cloudinary.com https://www.facebook.com https://www.google-analytics.com https://www.googletagmanager.com; "
-    "connect-src 'self' https://api.mercadopago.com https://*.mercadopago.com https://www.facebook.com https://graph.facebook.com https://www.google-analytics.com https://analytics.google.com https://challenges.cloudflare.com; "
+    "connect-src 'self' https://api.mercadopago.com https://*.mercadopago.com https://viacep.com.br https://www.facebook.com https://graph.facebook.com https://www.google-analytics.com https://analytics.google.com https://challenges.cloudflare.com; "
     "frame-src 'self' https://*.mercadopago.com https://www.mercadopago.com https://www.mercadopago.com.br https://challenges.cloudflare.com; "
     "form-action 'self' https://*.mercadopago.com https://www.mercadopago.com https://www.mercadopago.com.br"
 ).strip()
